2619eee3a8bac93f5512177cfb87404b63c7c714
Glourvell <149944607+Glourvell@users.noreply.github.com>
2026-01-14 11:40:41 +0300

    Refactor server.js to use ES modules and improve structure

--- server.js
+++ server.js
@@ -1,33 +1,77 @@
-{
-  "name": "cpesa",
-  "version": "1.0.0",
-  "description": "seamless AVAX purchase via M-pesa",
-  "main": "server.js",
-  "type": "module",
-  "engines": {
-    "node": "20.x"
-  },
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1",
-    "start": "node server.js"
-  },
-  "repository": {
-    "type": "git",
-    "url": "C-PESA"
-  },
-  "author": "glourvell",
-  "license": "ISC",
-  "dependencies": {
-    "axios": "^1.13.1",
-    "body-parser": "^2.2.0",
-    "dotenv": "^16.6.1",
-    "ejs": "^3.1.10",
-    "ethers": "^6.15.0",
-    "express": "^5.1.0",
-    "mongoose": "^8.21.0",
-    "ngrok": "^5.0.0-beta.2",
-    "path": "^0.12.7",
-    "socket.io": "^4.8.1",
-    "url": "^0.11.4"
-  }
-}
+import express from 'express';
+import axios from 'axios';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import router from './controllers/lipaNaMpesa.js'
+import callback from './controllers/lipaCallback.js';
+import mongoose from "mongoose";
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Error:", err));
+
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.json());
+
+
+app.use(router);
+app.use(callback);
+
+
+
+
+// app.use(mpesastk);
+app.use(callback);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongoooo is on");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Server is up and running");
+    });
+  })
+  .catch((err) => console.error("Mongo error:", err));
+
+
+
+
+
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.get("/", async(req, res) => {
+  res.render('payment', {failedMessage: null, successMessage: null});
+  });
+
+  app.get("/dashboard", async(req, res) => {
+    res.render('dashboard');
+    });
+
+app.listen(port,  () => {
+
+  console.log(`Server running on port ${port}`);
+
+  });
+  
